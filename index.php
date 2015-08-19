fd0ed3e179c833a63a4758d07878d8dc5ebb1019
Kursad Oney <kuoney@gmail.com>
2015-08-19 09:11:26 -0400

    New Design: Stgart with index.php template.
    In the new design, I plan to do the following:
    
    * Unify CSS
    * Just make the whole directory traversing thing neater
    * More dynamic content generation
    * Instead of scanning every file every time, store info in sqlite3
    * Better directory organization

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><HTML><HEAD>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso 8859-1">
+<title>FAE Documents Web Server Powered by Abyss</title>
+<link rel="stylesheet" type="text/css" href="docs.css">
+</head>
+
+<body>
+<?php
+echo "<p> Welcome to the new page! </p>";
+?>
+</body></html>
