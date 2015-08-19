7dce77007f2d6cc48a2cef891eda402ca19085eb
Kursad Oney <kuoney@gmail.com>
2015-08-19 17:18:27 -0400

    Update.php: Add preliminary support to update the links.
    This commit creates a database, db/docs.db, which contains
    the following information:
    
    	link, description, date, checksum
    
    When the update function is called, we check if the sha1 of the
    file exists in the current database. If so, there is no need to
    update. If not, we add a new row with the relevant information.
    
    So far, only the path name (no link) and the sha1 information
    is valid. The rest will come later.

--- php/update.php
+++ php/update.php
@@ -1,19 +1,19 @@
 <style> <?php include "../docs.css"; ?> </style>
 <?php
 
-function update_db ( $file )
+function update_db ($path, $db )
 {
-/*	if (checksum($file) not equal checksum_from_db()) {
-		exif_tool(file);
-		add_to_db(file_output);
+	$sha1 = sha1_file($path);
+
+	if (empty($db->querySingle("select checksum from documents where checksum=\"$sha1\""))) {
+		/* TODO: exiftool */
+		$db->exec("insert into documents values (\"$path\", \"D\", \"14\", \"$sha1\")");
 		return 1;
 	}
- */
-	print "<p>$file</p>";
 	return 0;
 }
 
-function update ($dir)
+function update ($dir, $db)
 {
 	$updated = 0;
 	$files = scandir($dir);
@@ -23,10 +23,10 @@ function update ($dir)
 			continue;
 
 		if (is_dir($dir . '/' . $file)) {
-			update($dir . '/' . $file);
+			$updated += update($dir . '/' . $file, $db);
 		} else {
 			if (substr($file, -3) === "pdf")
-				$updated += update_db($file);
+				$updated += update_db($dir . '/' . $file, $db);
 		}
 
 	}
@@ -36,7 +36,30 @@ function update ($dir)
 function main ()
 {
 	print("<p>Updating...</p>\n");
-	$count = update(dirname(__FILE__) . '/../docs');
+
+	$db = new SQLite3(dirname(__FILE__) . '/../db/docs.db');
+
+	/* Create the table if it doesn't exist */
+	$db->exec('create table if not exists documents(
+			link, description, date, checksum)');
+
+	$count = update(dirname(__FILE__) . '/../docs', $db);
+
+	echo '<hr><table id="links" class="gradienttable"><thead>';
+
+	echo '<th> Link </th><th>Description</th><th>Date</th><th>Checksum</th>',
+		'</thead><tbody>';
+	$results = $db->query('select * from documents');
+
+	while ($row = $results->fetchArray(SQLITE3_NUM)) {
+		echo ('<tr>');
+		foreach ($row as $cell) {
+			echo('<td>' . $cell . '</td>');
+		}
+		echo ('</tr>');
+	}
+	$db->close();
+
 	print("<p>Done. Updated $count files.</p>\n");
 }
 
