a0d9bc90863005940d6d2057e4f772e8a69112da
Kursad Oney <kuoney@gmail.com>
2015-08-20 10:13:10 -0400

    Update: Finish update procedure.
    In this commit, we retrieve the description and the date from the
    PDF file using exiftool and commit it to the database.
    
    We also increased the database columns to include some more data.
    The new format is:
    
    	link, path, dir, filename, description, date, checksum
    
    Single quotes are cleaned up from the description since they
    give the sqlite exec statement trouble.

--- php/update.php
+++ php/update.php
@@ -6,8 +6,33 @@ function update_db ($path, $db )
 	$sha1 = sha1_file($path);
 
 	if (empty($db->querySingle("select checksum from documents where checksum=\"$sha1\""))) {
-		/* TODO: exiftool */
-		$db->exec("insert into documents values (\"$path\", \"D\", \"14\", \"$sha1\")");
+		/* Figure out the relative link to the file */
+		$link = substr_replace($path, '/', 0, strlen($GLOBALS["TOPDIR"]));
+		$link = '<a href="' . $link . '">[pdf]</a><br>';
+
+		/* split file name and directory from path */
+		$fn = basename($path);
+		$dir = dirname($path);
+
+		/*
+		 * Run exiftool to get the description and the date from
+		 * metadata
+		 */
+		$cmd = '"' . $GLOBALS["TOPDIR"] . 'tools/exiftool.exe"' . " -m -f -CreateDate -Title -n -e -php \"$path\"";
+		eval('$filedata = ' . `$cmd`);
+
+		$date = 0;
+		$desc = 0;
+		foreach ($filedata as $i => $filex) {
+			$desc = $filex["Title"];
+			$desc = str_replace("'", "", $desc);
+			$date = $filex["CreateDate"];
+		}
+
+		$ret = $db->exec("insert into documents values ('$link', '$path', '$dir', '$fn',
+				'$desc', '$date', '$sha1')");
+		if (!$ret)
+			print "<p> XXX $desc $date $path </p>";
 		return 1;
 	}
 	return 0;
@@ -35,19 +60,17 @@ function update ($dir, $db)
 
 function main ()
 {
-	print("<p>Updating...</p>\n");
-
-	$db = new SQLite3(dirname(__FILE__) . '/../db/docs.db');
+	$db = new SQLite3($GLOBALS["TOPDIR"] . 'db/docs.db');
 
 	/* Create the table if it doesn't exist */
 	$db->exec('create table if not exists documents(
-			link, description, date, checksum)');
+			link, path, dir, fn, description, date, checksum)');
 
-	$count = update(dirname(__FILE__) . '/../docs', $db);
+	$count = update($GLOBALS["TOPDIR"] . 'docs', $db);
 
 	echo '<hr><table id="links" class="gradienttable"><thead>';
 
-	echo '<th> Link </th><th>Description</th><th>Date</th><th>Checksum</th>',
+	echo '<th> Link </th><th>Path</th><th>dir</th><th>filename</th><th>Description</th><th>Date</th><th>Checksum</th>',
 		'</thead><tbody>';
 	$results = $db->query('select * from documents');
 
@@ -63,5 +86,7 @@ function main ()
 	print("<p>Done. Updated $count files.</p>\n");
 }
 
+$TOPDIR = dirname(dirname(__FILE__)) . '/';
+$TOPDIR = str_replace('\\', '/', $TOPDIR);
 main();
 ?>
